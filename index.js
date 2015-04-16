c92515963db0a8bd5bf6f9f7cbe34d9a3cbe0081
Olivier Lalonde <olalonde@gmail.com>
2015-04-17 01:23:42 +0800

    Return function returning promise instead of promise object

--- index.js
+++ index.js
@@ -10,18 +10,21 @@ module.exports = function (opts, getPromise) {
   opts.max = opts.max || 10;
   opts.backoff = opts.backoff || 1000;
 
-  return new BPromise(function (resolve, reject) {
-    var attempt = function (i) {
-      getPromise(i).then(resolve).catch(function (err) {
-        if (i >= opts.max) {
-          return reject(err);
-        }
-        setTimeout(function () {
-          attempt(i+1);
-        }, i * opts.backoff);
-      });
-    };
+  // TODO: pass arguments down
+  return function () {
+    return new BPromise(function (resolve, reject) {
+      var attempt = function (i) {
+        getPromise(i).then(resolve).catch(function (err) {
+          if (i >= opts.max) {
+            return reject(err);
+          }
+          setTimeout(function () {
+            attempt(i+1);
+          }, i * opts.backoff);
+        });
+      };
 
-    attempt(0);
-  });
+      attempt(1);
+    });
+  };
 };
