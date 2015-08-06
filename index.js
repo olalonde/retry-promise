fb87056695e1816e68e842dd520ce61c5fc99e5a
Olivier Lalonde <olalonde@gmail.com>
2015-08-06 19:57:07 +0800

    Call retry function immediately

--- index.js
+++ index.js
@@ -11,20 +11,18 @@ module.exports = function (opts, getPromise) {
   opts.backoff = opts.backoff || 1000;
 
   // TODO: pass arguments down
-  return function () {
-    return new BPromise(function (resolve, reject) {
-      var attempt = function (i) {
-        getPromise(i).then(resolve).catch(function (err) {
-          if (i >= opts.max) {
-            return reject(err);
-          }
-          setTimeout(function () {
-            attempt(i+1);
-          }, i * opts.backoff);
-        });
-      };
+  return new BPromise(function (resolve, reject) {
+    var attempt = function (i) {
+      getPromise(i).then(resolve).catch(function (err) {
+        if (i >= opts.max) {
+          return reject(err);
+        }
+        setTimeout(function () {
+          attempt(i+1);
+        }, i * opts.backoff);
+      });
+    };
 
-      attempt(1);
-    });
-  };
+    attempt(1);
+  });
 };
