7cf0de7c43198d1ca50f7528477b960b97b2a280
Olivier Lalonde <olalonde@gmail.com>
2015-11-18 19:06:46 +0800

    refactor(es6): Move code to es6. Add more tests. BREAKING CHANGE.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const retry = (opts = {}, getPromise) => {
+  if (typeof opts === 'function') {
+    return retry({}, opts);
+  }
+
+  opts.max = opts.max || 10;
+  opts.backoff = opts.backoff || 1000;
+
+  return new Promise((resolve, reject) => {
+    const attempt = (i) => {
+      getPromise(i)
+        .then(resolve)
+        .catch((err) => {
+          if (i >= opts.max) {
+            return reject(err);
+          }
+          setTimeout(() => attempt(i + 1), i * opts.backoff);
+        });
+    };
+    attempt(1);
+  });
+};
+
+export default retry;
--- test/index.js
+++ test/index.js
@@ -1,37 +1,62 @@
-var expect = require('chai').expect;
-var retry = require('../');
+import test from 'blue-tape';
+import retry from '../src';
 
-describe('retry', function () {
-  it('successful at first attempt', function () {
-    var i;
-    return retry(function (attempt) {
-      i = attempt;
-      return new Promise(function (resolve, reject) {
-        process.nextTick(function () {
-          resolve('something');
-        });
+test('retry success', (t) => {
+  let i;
+  return retry((attempt) => {
+    i = attempt;
+    return new Promise((resolve) => {
+      process.nextTick(() => {
+        resolve('something');
       });
-    })
-    .then(function (res) {
-      expect(res).to.equal('something');
-      expect(i).to.equal(1);
     });
+  })
+  .then((res) => {
+    t.equal(res, 'something');
+    t.equal(i, 1);
   });
-
-  it('unsuccessful', function () {
-    var i;
-    return retry({ max: 2, backoff: 20 }, function (attempt) {
-      i = attempt;
-      return new Promise(function (resolve, reject) {
-        process.nextTick(function () {
-          reject('some err');
-        });
-      });
-    })
-    .catch(function (err) {
-      expect(i).to.equal(2);
-      expect(err).to.equal('some err');
-    });
-  });
-
 });
+
+test('retry fail', (t) => {
+  let i;
+  return retry({ max: 1 }, (attempt) => {
+    i = attempt;
+    return new Promise((resolve, reject) => {
+      process.nextTick(() => {
+        reject('some err');
+      });
+    });
+  })
+  .catch((err) => {
+    t.equal(err, 'some err');
+    t.equal(i, 1);
+  });
+});
+
+test('retry max', (t) => {
+  let i;
+  return retry({ max: 3, backoff: 5 }, (attempt) => {
+    i = attempt;
+    return Promise.reject('some err');
+  })
+  .catch((err) => {
+    t.equal(err, 'some err');
+    t.equal(i, 3);
+  });
+});
+
+test('retry few times before max', (t) => {
+  let i;
+  return retry({ max: 5, backoff: 5 }, (attempt) => {
+    i = attempt;
+    if (i === 4) {
+      return Promise.resolve('yay');
+    }
+    return Promise.reject('some err');
+  })
+  .then((res) => {
+    t.equal(res, 'yay');
+    t.equal(i, 4);
+  });
+});
+
